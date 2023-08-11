0ca417e632268f0e4f1e0c4d17caa1cc2dec2edc
JoelVStan <joelvstan@gmail.com>
2023-08-11 21:41:40 +0530

    The app is ready

--- Character.js
+++ Character.js
@@ -1,31 +1,47 @@
-import { getDiceRollArray } from './utils.js'
+import { getDiceRollArray, getDicePlaceholderHtml, getPercentage } from './utils.js'
 
 // constructor
-function Character(data) {
-    // this.elementId = data.elementId
-    // this.name = data.name
-    // this.avatar = data.avatar
-    // this.health = data.health
-    // this.diceCount = data.diceCount
-    Object.assign(this, data)
+class Character {
+    constructor(data) {
+        Object.assign(this, data)
 
-    this.getDiceHtml = function (diceCount){
-        return getDiceRollArray(diceCount).map(function(num){
-            return `<div class="dice">${num}</div>`
-        }).join('')
+        this.diceHtml = getDicePlaceholderHtml(this.diceCount)
+        this.maxHealth = this.health
     }
 
-    this.getCharacterHtml = function() {
-        const {elementId, name, avatar, health, diceCount} = this
+    setDiceHtml() {
+        this.currentDiceScore = getDiceRollArray(this.diceCount)
+        this.diceHtml = this.currentDiceScore.map( (num) => `<div class="dice">${num}</div>` ).join('')
+    }
 
-        const diceHtml = this.getDiceHtml(diceCount)
-        // .join() above used to avoid commas
-    
+    takeDamage(attackScoreArray) {
+        const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
+        this.health -= totalAttackScore 
+        if(this.health <= 0) {
+            this.dead = true
+            this.health = 0
+        }
+    }
+
+    getHealthBarHtml(){
+        const percent = getPercentage(this.health, this.maxHealth)
+        return `
+            <div class="health-bar-outer">
+                <div class="health-bar-inner ${percent < 26 ? "danger" : ""} " 
+                style="width: ${percent}%;">
+                </div>
+            </div>`
+    }
+
+    getCharacterHtml() {
+        const {elementId, name, avatar, health, diceCount, diceHtml} = this 
+        const healthBar = this.getHealthBarHtml()
         return `
             <div class="character-card">
                 <h4 class="name"> ${name} </h4>
                 <img class="avatar" src="${avatar}"/>
-                <p class="health">health: <b> ${health} </b></p>
+                <div class="health">health: <b> ${health} </b></div>
+                ${healthBar}
                 <div class="dice-container">
                     ${diceHtml} 
                 </div>
